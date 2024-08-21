8fe60184b71df62016c27cb90481ed2a01f17316
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2024-08-21 18:05:38 +0530

    ✨ Adding Ratings and Released Date

--- movie-tv-database/src/components/Movie.js
+++ movie-tv-database/src/components/Movie.js
@@ -1,5 +1,5 @@
 import React, { useEffect , useState } from 'react'
-
+import './Movie.css';
 function Movie() {
 
     const [movieList,setMovieList] = useState([])
@@ -19,11 +19,21 @@ function Movie() {
 
   return (
     <div>
+    <h1 className='heading'>
+        Movie Ratings Blog
+      </h1>
+    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+      
       {movieList.map((movie)=>(
-        
-        <img style ={{width:"380px",height:"250px"}} key={movie.id} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+        <div key={movie.id} style={{ marginBottom: '20px' }}>
+        <img style ={{width:"300px",height:"250px"}} key={movie.id} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+        <h3>{movie.title}</h3>
+          <p>Rating: {movie.vote_average}</p>
+          <p>Released: {movie.release_date}</p>
+        </div>
       ))}
       
+    </div>
     </div>
   )
 }
